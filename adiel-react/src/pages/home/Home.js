dde5166b30f744287cfe638c3a1e66a9af396320
Adiel <adieln00@gmail.com>
2024-10-01 07:33:13 +0300

    feat: another features

--- adiel-react/src/pages/home/Home.js
+++
@@ -1,7 +0,0 @@
-import Topbar from "../../components/top/Topbar";
-
-export default function Home() {
-    return(
-       <Topbar/>
-    )
-}
--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose').default;
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
-const postRoute = require(".routes/post")
+const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
 
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-).then(() =>
-    console.log("Connected to MongoDB")
-);
+const mongoURL = process.env.MONGO_URL;
 
-//middleware
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1); // Exit the process with failure
+    }
+};
+
+// Call the function to connect to MongoDB
+connectDB();
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploaded successfully");
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json("File upload failed");
+    }
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-app.use("/api/post", postRoute)
+app.use("/api/posts", postRoute);
 
-app.listen(8080, () => {
+app.listen(8800, () => {
     console.log("Backend server is running!");
 });
